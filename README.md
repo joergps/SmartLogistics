b8e15cae6dec79504ebf473e36b9c8379e6e5f1f
Pancake-Steeg, Jörg (U521288) <joerg.pancake-steeg@lhsystems.com>
2016-08-11 08:14:27 +0200

    Added simple page that can be set to display status of an item.
    Change-Id: I7409a5c0cdf0f18d082e49bcd8c6e8cb8a888543

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SmartLogistics
 Code related to the SmartLogistics project.
+
+# Java Web-Gui
+Adresse: http://localhost:8080
+
+Nachricht, dass ein Packstück da ist: http://localhost:8080?status=da
--- src/java/smartlogistics/src/main/java/aero/ltls/SmartLogisticsController.java
+++ src/java/smartlogistics/src/main/java/aero/ltls/SmartLogisticsController.java
@@ -8,10 +8,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class SmartLogisticsController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    /** File name (w/o extension) of the template for the index page. */
+    private static final String INDEX_PAGE_FILE_NAME = "index";
+
+    /** Name for the REST parameter "hasItem". */
+    private static final String PARAM_NAME_HAS_ITEM = "hasItem";
+
+    /**
+     * Map the following REST endpoint to "/".
+     *
+     * @param status
+     * @param model
+     * @return a {@link String} that represents the template to be used
+     */
+    @RequestMapping("/")
+    public String index(@RequestParam(value=PARAM_NAME_HAS_ITEM, required=false, defaultValue="false") boolean hasItem, Model model) {
+        model.addAttribute(PARAM_NAME_HAS_ITEM, hasItem);
+        return INDEX_PAGE_FILE_NAME;
     }
 
 }
