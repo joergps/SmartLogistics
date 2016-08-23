a7cd19c605b796e92a717d10983702bcd5cff4a4
joergps <joerg.pancake-steeg@lhsystems.com>
2016-08-23 20:28:10 +0000

    Script sends REST-call if item is present.

--- src/py/04_Ultraschall/04_Ultraschall.py
+++ src/py/04_Ultraschall/04_Ultraschall.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 # Taken from http://www.einplatinencomputer.com/raspberry-pi-ultraschallsensor-hc-sr04-ansteuern-entfernung-messen/
 import RPi.GPIO as GPIO
+import requests
 import time
 
 print 'Starting 04_Ultraschall.py'
 
 # define constants
+NO_ELEMENT_DISTANCE = 210 # distance in cm that is returned if no element in front of sensor
 SONIC_SPEED = 34300 # sonic speed in cm/s
 TRIGGER_TIME = 0.0001 # length of ultra sound trigger in seconds
 
 # define pins
 pinTrigger = 11 # pin 11 = GPIO 17
 pinEcho = 18 # pin 18 = GPIO 24
+
+# define REST calls
+restUri = 'http://localhost:8080'
+restParamHasItem = { 'hasItem': 'true' }
+restParamNoItem = { 'hasItem': 'false' }
 
 # access numbers by physical location
 GPIO.setmode(GPIO.BOARD)
@@ -48,7 +55,12 @@ if __name__ == '__main__':
 	try:
 		while True:
 			distance = getDistance()
-			print 'Distanz = %.1f cm' % distance
+			if (distance < NO_ELEMENT_DISTANCE): 
+				print 'Distanz = %.1f cm' % distance
+				r = requests.get(restUri, params=restParamHasItem)
+				# print r.text
+			else:
+				requests.get(restUri, params=restParamNoItem)
 			time.sleep(1)
 
 	except KeyboardInterrupt:
