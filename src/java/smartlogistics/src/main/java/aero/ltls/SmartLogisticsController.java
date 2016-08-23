5350a74da337cb483a417c65b0886963b8a46349
Pancake-Steeg, Jörg (U521288) <joerg.pancake-steeg@lhsystems.com>
2016-08-23 10:39:46 +0200

    Controller maintains item stage such that state can be changed with a REST call.
    Change-Id: I648ab46180ec673585b39f4196ed468948ace586

--- src/java/smartlogistics/src/main/java/aero/ltls/SmartLogisticsController.java
+++ src/java/smartlogistics/src/main/java/aero/ltls/SmartLogisticsController.java
@@ -14,6 +14,9 @@ public class SmartLogisticsController {
     /** Name for the REST parameter "hasItem". */
     private static final String PARAM_NAME_HAS_ITEM = "hasItem";
 
+	/** State whether the station has an item or not. **/
+	private boolean hasItem = false;
+
     /**
      * Map the following REST endpoint to "/".
      *
@@ -22,9 +25,34 @@ public class SmartLogisticsController {
      * @return a {@link String} that represents the template to be used
      */
     @RequestMapping("/")
-    public String index(@RequestParam(value=PARAM_NAME_HAS_ITEM, required=false, defaultValue="false") boolean hasItem, Model model) {
-        model.addAttribute(PARAM_NAME_HAS_ITEM, hasItem);
+	public String index(@RequestParam(value = PARAM_NAME_HAS_ITEM, required = false) Boolean hasItemCurrently,
+			Model model) {
+		setHasItem(hasItemCurrently);
+		model.addAttribute(PARAM_NAME_HAS_ITEM, isHasItem());
         return INDEX_PAGE_FILE_NAME;
     }
+
+	/**
+	 * Getter for {@link #hasItem}.
+	 *
+	 * @return <code>true</code> if station has an item; <code>false</code>
+	 *         otherwise.
+	 */
+	private boolean isHasItem() {
+		return hasItem;
+	}
+
+	/**
+	 * Setter for {@link #hasItem}.
+	 *
+	 * @param hasItemCurrently
+	 *            a {@link Boolean}; if <code>null</code> then the state is not
+	 *            changed
+	 */
+	private void setHasItem(Boolean hasItemCurrently) {
+		if (hasItemCurrently != null) {
+			this.hasItem = hasItemCurrently.booleanValue();
+		}
+	}
 
 }
