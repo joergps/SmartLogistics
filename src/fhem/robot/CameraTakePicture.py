6f959ee0d71d6f99917d4eedb7c07ba9ab24b935
joergps <joerg.pancake-steeg@lhsystems.com>
2016-09-07 09:36:43 +0000

    Take picture when distance < 40cm.

--- src/fhem/robot/CameraTakePicture.py
+++ src/fhem/robot/CameraTakePicture.py
@@ -5,13 +5,16 @@ from time import sleep
 from time import time
 import os
 
+sleep(1)
+
 camera = PiCamera()
 camera.resolution = (1024, 768)
 # camera.start_preview()
 
 # Camera warm-up time
-sleep(3)
-camera.capture('/opt/fhem/www/tablet/image.jpg')
+sleep(2)
+# camera.capture('/opt/fhem/www/tablet/images/image.jpg')
+camera.capture('image.jpg')
 url='curl http://localhost:8083/fhem?cmd=setreading%20picture%20current_file%20image.jpg'
 currentTime=time()
 os.system(url + '?' + str(currentTime))
